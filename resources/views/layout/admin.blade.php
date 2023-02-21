333f4fb25d27c7a657b54fe857c0814aab2320a2
Ali Zulfikar <62384197+alizul01@users.noreply.github.com>
2023-02-21 14:54:16 +0700

    Project Scaffold:<TailwindCSS,SweetAlert2,Laravel>

---
+++ resources/views/layout/admin.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.master')
+
+@section('master')
+
+@endsection
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    @include('sweetalert::alert')
+    @yield('master')
+</body>
+
+</html>
