f40490a235b0e09a70adfbd89de81e4fbf5bd2cd
alizul01 <alizulfikar032@gmail.com>
2023-03-29 10:52:43 +0700

    fix: cypress e2e testing, move: tests/

--- database/factories/RoomFactory.php
+++ database/factories/RoomFactory.php
@@ -16,8 +16,21 @@ class RoomFactory extends Factory
      */
     public function definition(): array
     {
+        $roomName = [
+            'LSI',
+            'LKJ',
+            'LPY',
+            'RT',
+            'LPR',
+        ];
+
+        // faker random element from array but give number for the element, ex: LSI-1, and it will be unique, so it will not repeat
+
         return [
-            
+            'name' => $this->faker->name,
+            'room_name' => $roomName[$this->faker->numberBetween(0, 4)] . '-' . $this->faker->unique()->numberBetween(1, 100),
+            'capacity' => $this->faker->numberBetween(30, 60),
+            'is_available' => $this->faker->boolean,
         ];
     }
 }
