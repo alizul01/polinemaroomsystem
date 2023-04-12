7348827bcde766f3d8299e5da7fa9ed8a3911197
alfanalivan <alfanalivan@gmail.com>
2023-04-12 08:38:49 +0700

    feat: slicing auth pages (1/2)

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,15 +1,28 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="icon" type="image/x-icon" href="{{ asset('img/logo.svg') }}">
+  <title>Polarys | Polinema Room System</title>
+
+  {{-- Link Font --}}
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet">
+  {{-- Link Css --}}
+  @vite('resources/css/app.css')
 </head>
 
-<body>
-    @include('sweetalert::alert')
-    @yield('master')
+<body class="font-sans">
+  @include('sweetalert::alert')
+  @yield('master')
+  {{-- Link Js --}}
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js"></script>
 </body>
 
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
-    theme: {
-        extend: {},
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
     },
-    plugins: [require("tailwindcss-plugins/pagination")],
+    extend: {},
+  },
+  plugins: [
+    require("tailwindcss-plugins/pagination"),
+    require("flowbite/plugin"),
+  ],
 };
