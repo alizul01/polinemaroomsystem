aab970537ef1e909fd147fa43df324bc3fc3811c
alizul01 <alizulfikar032@gmail.com>
2023-04-20 15:37:58 +0700

    feat(auth): work register and login authentication system

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ApprovalController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,17 +20,26 @@ Route::group(['middleware' => ['auth', 'admin']], function () {
   Route::get('/admin', function () {
     return response('Admin', 200);
   });
-
   Route::resource('approval', ApprovalController::class)->parameter('approval', 'id');
+});
+
+Route::group(['middleware' => ['auth']], function () {
+  Route::get('/', function () {
+    return view('index');
+  });
+  Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 });
 
 // user guest
 Route::group(['middleware' => ['guest']], function () {
-    Route::get('/login', function () {
-        return view('auth.login');
-    });
+  Route::get('/login', function () {
+    return view('auth.login');
+  })->name('login');
 
   Route::get('/register', function () {
     return view('auth.register');
-  });
+  })->name('register');
+
+  Route::post('/login', [AuthController::class, 'login']);
+  Route::post('/register', [AuthController::class, 'register']);
 });
