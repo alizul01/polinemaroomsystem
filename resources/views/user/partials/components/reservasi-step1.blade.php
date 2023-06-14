29ec9955a516c1b6fcefc2087b4e4a33cb88748b
alfanalivan <alfanalivan@gmail.com>
2023-06-14 11:43:59 +0700

    feat: dropdown logout

--- resources/views/user/partials/components/reservasi-step1.blade.php
+++ resources/views/user/partials/components/reservasi-step1.blade.php
@@ -1,116 +1,133 @@
 @extends('user.reservasi')
 
 @section('content')
-    <section class="flex flex-col grow p-6 bg-white border-2 rounded-lg shadow-md min-w-fit">
-        <form class="flex flex-col gap-9" action="{{ route('reservation.store') }}" method="POST">
-            @csrf
-            <div class="flex flex-col gap-4">
-                <h1 class="text-xl font-medium">Data Peminjam</h1>
-                <div class="flex gap-5">
-                    <div class="flex-1">
-                        <label for="user_id" class="block mb-2 text-sm text-gray-600">Nama Lengkap
-                            <span class="text-red-600">*</span></label>
-                        <input type="text" name="user_id" id="user_id" autocomplete="given-name" autofocus
-                            class="w-full py-3 bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500" value="{{ Auth::user()->name }}" disabled>
-                        @error('user_id')
-                            <span class="text-red-600">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="flex-1">
-                        <label for="nomor_induk" class="block mb-2 text-sm text-gray-600">Nomor Induk
-                            <span class="text-red-600">*</span></label>
-                        <input type="text" name="nomor_induk" id="nomor_induk" autocomplete="given-nomor_induk"
-                            class="w-full py-3 bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500" value="{{ Auth::user()->nomor_induk }}" disabled>
-                        @error('nomor_induk')
-                            <span class="text-red-600">{{ $message }}</span>
-                        @enderror
-                    </div>
-                </div>
-                <div class="flex gap-5">
-                    <div class="flex-1">
-                        <label for="organization_id" class="block mb-2 text-sm text-gray-600">Asal UKM/OKI
-                            <span class="text-red-600">*</span></label>
-                        <select name="organization_id" id="organization_id"
-                            class="w-full py-3 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500">
-                            @foreach ($organizations as $organization)
-                                <option value="{{ $organization->id }}">{{ $organization->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="flex-1">
-                        <label for="phonenumber" class="block mb-2 text-sm text-gray-600">Nomor Telefon
-                            <span class="text-red-600">*</span></label>
-                        <input type="text" name="phonenumber" id="phonenumber" autocomplete="given-phonenumber"
-                            class="w-full py-3 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500">
-                        @error('phonenumber')
-                            <span class="text-red-600">{{ $message }}</span>
-                        @enderror
-                    </div>
-                </div>
-            </div>
-            <div class="flex flex-col gap-4">
-                <h1 class="text-xl font-medium">Data Peminjaman</h1>
-                <div class="flex gap-5">
-                    <div class="flex-1">
-                        <label for="start_date" class="block mb-2 text-sm text-gray-600">Tanggal Peminjaman
-                            <span class="text-red-600">*</span></label>
-                        <input type="date" name="start_date" id="start_date" autocomplete="given-start_date"
-                            class="w-full py-3 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500">
-                        @error('start_date')
-                            <span class="text-red-600">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="flex-1">
-                        <label for="start_time" class="block mb-2 text-sm text-gray-600">Jam Mulai
-                            <span class="text-red-600">*</span></label>
-                        <input type="time" name="start_time" id="start_time" autocomplete="given-start_time"
-                            class="w-full py-3 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500">
-                        @error('start_time')
-                            <span class="text-red-600">{{ $message }}</span>
-                        @enderror
-                    </div>
-                </div>
-                <div class="flex gap-5">
-                    <div class="flex-1">
-                        <label for="end_time" class="block mb-2 text-sm text-gray-600">Jam Selesai
-                            <span class="text-red-600">*</span></label>
-                        <input type="time" name="end_time" id="end_time" autocomplete="given-end_time"
-                            class="w-full py-3 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500">
-                        @error('end_time')
-                            <span class="text-red-600">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="flex-1">
-                        <label for="participant" class="block mb-2 text-sm text-gray-600">Jumlah Orang</label>
-                        <input type="number" name="participant" id="participant" autocomplete="given-participant"
-                            class="w-full py-3 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500">
-                        @error('participant')
-                            <span class="text-red-600">{{ $message }}</span>
-                        @enderror
-                    </div>
-                </div>
-            </div>
-            <div class="flex flex-col gap-4">
-                <h1 class="text-xl font-medium">Keterangan</h1>
-                <div class="flex gap-5">
-                    <div class="flex-1">
-                        <label for="keterangan" class="block mb-2 text-sm text-gray-600">Keterangan
-                            Tambahan</label>
-                        <textarea name="keterangan" id="keterangan" rows="4"
-                            class="w-full py-3 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500"></textarea>
-                        @error('keterangan')
-                            <span class="text-red-600">{{ $message }}</span>
-                        @enderror
-                    </div>
-                </div>
-            </div>
-            <div class="flex justify-between">
-                <span>Keterangan: <span class="text-red-600">*</span> harus diisi</span>
-                <button type="submit"
-                    class="flex gap-1 justify-center items-center text-white bg-gray-700 font-medium rounded-lg p-3 text-center hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-gray-100">
-                    Lanjutkan<i class="bx bx-right-arrow-alt text-xl"></i>
-                </button>
-            </div>
-        </form>
-    </section>
+  <section class="flex flex-col grow p-6 bg-white border-2 rounded-lg shadow-md min-w-fit">
+    <form class="flex flex-col gap-9" action="{{ route('reservation.store') }}" method="POST">
+      @csrf
+      <div class="flex flex-col gap-4">
+        <h1 class="text-xl font-medium">Data Ruangan</h1>
+        <div class="flex gap-5">
+          <div class="flex-1">
+            <label for="organization_id" class="block mb-2 text-sm text-gray-600">Ruangan
+              <span class="text-red-600">*</span></label>
+            <select name="organization_id" id="organization_id"
+              class="w-full py-3 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500">
+              @foreach ($organizations as $organization)
+                <option value="{{ $organization->id }}">{{ $organization->name }}</option>
+              @endforeach
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col gap-4">
+        <h1 class="text-xl font-medium">Data Peminjam</h1>
+        <div class="flex gap-5">
+          <div class="flex-1">
+            <label for="user_id" class="block mb-2 text-sm text-gray-600">Nama Lengkap
+              <span class="text-red-600">*</span></label>
+            <input type="text" name="user_id" id="user_id" autocomplete="given-name" autofocus
+              class="w-full py-3 bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500"
+              value="{{ Auth::user()->name }}" disabled>
+            @error('user_id')
+              <span class="text-red-600">{{ $message }}</span>
+            @enderror
+          </div>
+          <div class="flex-1">
+            <label for="nomor_induk" class="block mb-2 text-sm text-gray-600">Nomor Induk
+              <span class="text-red-600">*</span></label>
+            <input type="text" name="nomor_induk" id="nomor_induk" autocomplete="given-nomor_induk"
+              class="w-full py-3 bg-gray-300 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500"
+              value="{{ Auth::user()->nomor_induk }}" disabled>
+            @error('nomor_induk')
+              <span class="text-red-600">{{ $message }}</span>
+            @enderror
+          </div>
+        </div>
+        <div class="flex gap-5">
+          <div class="flex-1">
+            <label for="organization_id" class="block mb-2 text-sm text-gray-600">Asal UKM/OKI
+              <span class="text-red-600">*</span></label>
+            <select name="organization_id" id="organization_id"
+              class="w-full py-3 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500">
+              @foreach ($organizations as $organization)
+                <option value="{{ $organization->id }}">{{ $organization->name }}</option>
+              @endforeach
+            </select>
+          </div>
+          <div class="flex-1">
+            <label for="phonenumber" class="block mb-2 text-sm text-gray-600">Nomor Telefon
+              <span class="text-red-600">*</span></label>
+            <input type="text" name="phonenumber" id="phonenumber" autocomplete="given-phonenumber"
+              class="w-full py-3 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500">
+            @error('phonenumber')
+              <span class="text-red-600">{{ $message }}</span>
+            @enderror
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col gap-4">
+        <h1 class="text-xl font-medium">Data Peminjaman</h1>
+        <div class="flex gap-5">
+          <div class="flex-1">
+            <label for="start_date" class="block mb-2 text-sm text-gray-600">Tanggal Peminjaman
+              <span class="text-red-600">*</span></label>
+            <input type="date" name="start_date" id="start_date" autocomplete="given-start_date"
+              class="w-full py-3 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500">
+            @error('start_date')
+              <span class="text-red-600">{{ $message }}</span>
+            @enderror
+          </div>
+          <div class="flex-1">
+            <label for="start_time" class="block mb-2 text-sm text-gray-600">Jam Mulai
+              <span class="text-red-600">*</span></label>
+            <input type="time" name="start_time" id="start_time" autocomplete="given-start_time"
+              class="w-full py-3 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500">
+            @error('start_time')
+              <span class="text-red-600">{{ $message }}</span>
+            @enderror
+          </div>
+        </div>
+        <div class="flex gap-5">
+          <div class="flex-1">
+            <label for="end_time" class="block mb-2 text-sm text-gray-600">Jam Selesai
+              <span class="text-red-600">*</span></label>
+            <input type="time" name="end_time" id="end_time" autocomplete="given-end_time"
+              class="w-full py-3 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500">
+            @error('end_time')
+              <span class="text-red-600">{{ $message }}</span>
+            @enderror
+          </div>
+          <div class="flex-1">
+            <label for="participant" class="block mb-2 text-sm text-gray-600">Jumlah Orang</label>
+            <input type="number" name="participant" id="participant" autocomplete="given-participant"
+              class="w-full py-3 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500">
+            @error('participant')
+              <span class="text-red-600">{{ $message }}</span>
+            @enderror
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col gap-4">
+        <h1 class="text-xl font-medium">Keterangan</h1>
+        <div class="flex gap-5">
+          <div class="flex-1">
+            <label for="keterangan" class="block mb-2 text-sm text-gray-600">Keterangan
+              Tambahan</label>
+            <textarea name="keterangan" id="keterangan" rows="4"
+              class="w-full py-3 bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500"></textarea>
+            @error('keterangan')
+              <span class="text-red-600">{{ $message }}</span>
+            @enderror
+          </div>
+        </div>
+      </div>
+      <div class="flex justify-between">
+        <span>Keterangan: <span class="text-red-600">*</span> harus diisi</span>
+        <button type="submit"
+          class="flex gap-1 justify-center items-center text-white bg-gray-700 font-medium rounded-lg p-3 text-center hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-gray-100">
+          Lanjutkan<i class="bx bx-right-arrow-alt text-xl"></i>
+        </button>
+      </div>
+    </form>
+  </section>
 @endsection
--- resources/views/user/reservasi.blade.php
+++ resources/views/user/reservasi.blade.php
@@ -6,7 +6,8 @@
       class="flex flex-col max-h-[25rem] gap-8 p-6 bg-white border border-gray-200 rounded-lg shadow-md w-1/3">
       <h1 class="font-medium text-xl">Alur Peminjaman</h1>
       <div class="flex items-center gap-7">
-        <span class="text-white {{ $step == 1 ? 'bg-cyan-500' : 'bg-green-500' }} font-medium rounded-full text-lg py-3.5 px-6 text-center">1</span>
+        <span
+          class="text-white {{ $step == 1 ? 'bg-cyan-500' : 'bg-green-500' }} font-medium rounded-full text-lg py-3.5 px-6 text-center">1</span>
         <div class="flex flex-col gap-1">
           <h2 class="font-medium text-lg">Mengisi Data</h2>
           <p class="text-gray-500 text-sm">
@@ -15,16 +16,7 @@
         </div>
       </div>
       <div class="flex items-center gap-7">
-        <span class="{{ $step == 2 ? 'bg-cyan-500' : 'bg-gray-300' }} font-medium rounded-full text-lg py-3.5 px-6 text-center">2</span>
-        <div class="flex flex-col gap-1">
-          <h2 class="font-medium text-lg">Pilih Ruangan</h2>
-          <p class="text-gray-500 text-sm">
-            Pilih ruangan berdasarkan keperluan anda atau pilih ruangan dari saran kami
-          </p>
-        </div>
-      </div>
-      <div class="flex items-center gap-7">
-        <span class="bg-gray-300 font-medium rounded-full text-lg py-3.5 px-6 text-center">3</span>
+        <span class="bg-gray-300 font-medium rounded-full text-lg py-3.5 px-6 text-center">2</span>
         <div class="flex flex-col gap-1">
           <h2 class="font-medium text-lg">Form Final</h2>
           <p class="text-gray-500 text-sm">
